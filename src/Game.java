a2ff7e7209e3b3d46f30e266a12b4ce18a552ec7
m2nesic <michaelnesic@gmail.com>
2019-04-02 01:46:53 -0400

    JavaFx SetScene

---
+++ src/Game.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Game extends Application {
+
+
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("JavaFx-Game");
+
+        StackPane layout = new StackPane();
+
+        Scene scene = new Scene(layout,300,250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
